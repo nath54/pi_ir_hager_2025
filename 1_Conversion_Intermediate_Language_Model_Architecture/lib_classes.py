14dcd1b8485331172a6c5e2bc797c391f4dbdfc5
nath54 <cerisara.nathan@gmail.com>
2025-02-19 16:00:44 +0100

    Travail

--- 1_Conversion_Intermediate_Language_Model_Architecture/lib_classes.py
+++ 1_Conversion_Intermediate_Language_Model_Architecture/lib_classes.py
@@ -4,6 +4,117 @@ from typing import Any, Optional
 # NOTE: when saying variable type, if there is an iterable or a tensor, the shape must be indicated
 
 
+
+####################################################################
+#######################     EXPRESSIONS      #######################
+####################################################################
+
+
+class Expression:
+    #
+    def __init__(self) -> None:
+        # ABSTRACT CLASS
+        pass
+
+    #
+    def __repr__(self) -> str:
+        #
+        return self.__str__()
+
+    #
+    def __str__(self) -> str:
+        #
+        return "[PLACEHOLDER FOR EXPRESSION]"
+
+
+#
+class ExpressionVariable(Expression):
+    #
+    def __init__(self, var_name: str) -> None:
+        #
+        self.var_name: str = var_name
+
+    #
+    def __str__(self) -> str:
+        #
+        return f"{self.var_name}"
+
+
+#
+class ExpressionConstant(Expression):
+    #
+    def __init__(self, constant: str) -> None:
+        #
+        self.constant: str = constant
+
+    #
+    def __str__(self) -> str:
+        #
+        return f"{self.constant}"
+
+
+# Normally, with the following FlowControlInstructions basic instructions, there is no need to have anything else than ExpressionVariable and ExpressionConstant, because we can decompose any complex instructions in a sequence of basic instructions (we may have to create temporary variables, but aside that, it is good)
+
+
+####################################################################
+#######################      CONDITIONS      #######################
+####################################################################
+
+
+class Condition:
+    #
+    def __init__(self) -> None:
+        # ABSTRACT CLASS
+        pass
+
+    #
+    def __repr__(self) -> str:
+        #
+        return self.__str__()
+
+    #
+    def __str__(self) -> str:
+        #
+        return "[PLACEHOLDER FOR CONDITION]"
+
+    #
+    def __hash__(self) -> int:
+        #
+        return self.__str__().__hash__()
+
+
+#
+class ConditionBinary(Condition):
+    #
+    def __init__(self, elt1: Expression | Condition, cond_operator: str, elt2: Expression | Condition) -> None:
+        #
+        self.elt1: Expression | Condition = elt1
+        self.cond_operator: str = cond_operator  # Can be `and`, `or`, `>`, `<`, `>=`, `<=`, `!=`, `==`
+        self.elt2: Expression | Condition = elt2
+
+    #
+    def __str__(self) -> str:
+        #
+        return f"{self.elt1} {self.cond_operator} {self.elt2}"
+
+#
+class ConditionUnary(Condition):
+    #
+    def __init__(self, elt: Expression | Condition, cond_operator: Optional[str] = None) -> None:
+        #
+        self.elt: Expression | Condition = elt
+        self.cond_operator: Optional[str] = cond_operator   # Can be `not`
+
+    #
+    def __str__(self) -> str:
+        #
+        if self.cond_operator is None:
+            #
+            return f"{self.elt}"
+        #
+        return f"{self.cond_operator} {self.elt}"
+
+
 ##############################################################
 #######################     LAYERS     #######################
 ##############################################################
@@ -32,15 +143,25 @@ class Layer:
 #
 class LayerCondition(Layer):
     #
-    def __init__(self, layer_var_name: str, layer_conditions_blocks: dict[str, Layer]) -> None:
+    def __init__(self, layer_var_name: str, layer_conditions_blocks: dict[Condition, Layer]) -> None:
         #
         self.layer_var_name: str = layer_var_name
-        self.layer_conditions_blocks: dict[str, Layer] = layer_conditions_blocks  # The idea is that each str will be a condition (ex: fn(X) < 0.4), and this block will do an if, elif, elif, else, and returns the execution of the block corresponding to the validated condition.
+        self.layer_conditions_blocks: dict[Condition, Layer] = layer_conditions_blocks  # The idea is that each str will be a condition (ex: fn(X) < 0.4), and this block will do an if, elif, elif, else, and returns the execution of the block corresponding to the validated condition.
         # The idea of this layer / block, is to simplify the conditions in the forward pass
         # So there are constraints, like the fact that each of theses layers must have the same input shapes and same output shapes
         # The idea is to create the blocks when we see an if in a forward pass (so link to the layers that are used in the main block)
         # Okay, so, finally, there is no need to create completely new blocks, just sub block functions `BlockFunction`, so there is no issues of weights / layers duplications.
 
+    #
+    def __str__(self) -> str:
+        #
+        res: str = "\n\t* Layer Condition:\n"
+        #
+        for condition in self.layer_conditions_blocks:
+            #
+            res += f"\t\t- {condition} : {self.layer_conditions_blocks[condition]}"
+        #
+        return res
 
 
 ####################################################################
@@ -69,16 +190,88 @@ class FlowControlInstruction:
 #
 class FlowControlVariableInit(FlowControlInstruction):
     #
-    def __init__(self, var_name: str, var_type: str, var_value: Optional[Any] = None) -> None:
+    def __init__(self, var_name: str, var_type: str, var_value: Optional[Expression] = None) -> None:
         #
         self.var_name: str = var_name
         self.var_type: str = var_type
-        self.var_value: Optional[Any] = var_value
+        self.var_value: Optional[Expression] = var_value
 
     #
     def __str__(self) -> str:
         #
         return f"\t\t * {self.var_name}: {self.var_type} = {self.var_value}\n"
+
+
+#
+class FlowControlVariableAssignment(FlowControlInstruction):
+    #
+    def __init__(self, var_name: str, var_value: Expression) -> None:
+        #
+        self.var_name: str = var_name
+        self.var_value: Expression = var_value
+
+    #
+    def __str__(self) -> str:
+        #
+        return f"\t\t * {self.var_name} = {self.var_value}\n"
+
+#
+class FlowControlBasicBinaryArithmetic(FlowControlInstruction):
+    #
+    def __init__(self, output_var_name: str, input1_var_name: str, operation: str, input2_var_name: str) -> None:
+        #
+        self.output_var_name: str = output_var_name
+        self.input1_var_name: str = input1_var_name
+        self.input2_var_name: str = input2_var_name
+        self.operation: str = operation
+
+    #
+    def __str__(self) -> str:
+        #
+        return f"\t\t * {self.output_var_name} = {self.input1_var_name} {self.operation} {self.input2_var_name}"
+
+#
+class FlowControlBasicUnaryArithmetic(FlowControlInstruction):
+    #
+    def __init__(self, output_var_name: str, operation: str, input_var_name: str) -> None:
+        #
+        self.output_var_name: str = output_var_name
+        self.input_var_name: str = input_var_name
+        self.operation: str = operation
+
+    #
+    def __str__(self) -> str:
+        #
+        return f"\t\t * {self.output_var_name} = {self.operation} {self.input_var_name}"
+
+
+#
+class FlowControlForLoop(FlowControlInstruction):
+    #
+    def __init__(self, iterable_var_name: str, iterator: str, flow_control_instructions: list[FlowControlInstruction]) -> None:
+        #
+        self.iterable_var_name: str = iterable_var_name
+        self.iterator: str = iterator
+        self.flow_control_instructions: list[FlowControlInstruction] = flow_control_instructions
+
+    #
+    def __str__(self) -> str:
+        #
+        return f"\t\t * for {self.iterable_var_name} in {self.iterator} {{\n{"\n".join( [fci.__str__() for fci in self.flow_control_instructions] )}\n}}"
+
+
+#
+class FlowControlWhileLoop(FlowControlInstruction):
+    #
+    def __init__(self, condition: Condition, flow_control_instructions: list[FlowControlInstruction]) -> None:
+        #
+        self.condition: Condition = condition
+        self.flow_control_instructions: list[FlowControlInstruction] = flow_control_instructions
+
+    #
+    def __str__(self) -> str:
+        #
+        return f"\t\t * while {self.condition} {{\n{"\n".join( [fci.__str__() for fci in self.flow_control_instructions] )}\n}}"
 
 
 #
@@ -139,9 +332,6 @@ class FlowControlReturn(FlowControlInstruction):
         return f"\t\t * return {self.return_variables}"
 
 
-# TODO: Create mode FlowControl classes **if needed** for basic operations and variables manipulation, mathematical operations, and other stuff
-
-
 ##############################################################
 #######################     BLOCKS     #######################
 ##############################################################
@@ -196,3 +386,26 @@ class ModelBlock:
         return f"\n\nModelBlock:\n\t-block_name: {self.block_name}\n\t-block_parameters: {self.block_parameters}\n\t-block_layers: \n{"\n".join([self.block_layers[layer].__str__() for layer in self.block_layers])}\n\t-Functions:\n\n{"\n\n".join([fn_name + " = " + self.block_functions[fn_name].__str__() for fn_name in self.block_functions])}\n"
 
 
+
+#########################################################################
+#######################     MAIN OUTPUT CLASS     #######################
+#########################################################################
+
+
+#
+class MainModelAnalysis:
+    #
+    def __init__(self) -> None:
+        """
+        Output of the analysis.
+
+        Attributes:
+            - model_blocks (dict[str, lc.ModelBlock]): list of all the blocks analyzed here, indexed by their blocks id (e.g. their name)
+            - main_block (str): id of the main block, given in sys.argv with `--main-block <MainBlockName>`
+            - current_model_visit (list[str]): stack of all the current blocks we are working on currently, access to the top one with [-1]
+            - current_function_visit (str): name of the current visited function if we are
+        """
+        #
+        self.model_blocks: dict[str, ModelBlock] = {}
+        #
+        self.main_block: str = ""
--- 1_Conversion_Intermediate_Language_Model_Architecture/test_extraction.py
+++ 1_Conversion_Intermediate_Language_Model_Architecture/test_extraction.py
@@ -7,6 +7,7 @@ import os
 import lib_classes as lc
 
 # FOR DEBUGGING
+# The debug_var displays all the attributes of a variable
 sys.path.insert(0, "../debug/")
 #
 from lib_debug import debug_var  # type: ignore
@@ -24,6 +25,15 @@ class ModelAnalyzer(ast.NodeVisitor):
 
     #
     def __init__(self) -> None:
+        """
+        Initialiser of the method ModelAnalyzer
+
+        Attributes:
+            - model_blocks (dict[str, lc.ModelBlock]): list of all the blocks analyzed here, indexed by their blocks id (e.g. their name)
+            - main_block (str): id of the main block, given in sys.argv with `--main-block <MainBlockName>`
+            - current_model_visit (list[str]): stack of all the current blocks we are working on currently, access to the top one with [-1]
+            - current_function_visit (str): name of the current visited function if we are
+        """
         #
         self.model_blocks: dict[str, lc.ModelBlock] = {}
         #
@@ -39,6 +49,16 @@ class ModelAnalyzer(ast.NodeVisitor):
 
     #
     def _is_torch_module_class(self, node: ast.ClassDef) -> bool:
+        """
+        Indicates if the given ClassDef node is a nn.Module subClass or not.
+
+        Args:
+            node (ast.ClassDef): node to tell if subClass of nn.Module
+
+        Returns:
+            bool: True if subClass of nn.Module, else False
+        """
+
         for base in node.bases:
             if isinstance(base, ast.Attribute) and base.attr == "Module" and isinstance(base.value, ast.Name) and base.value.id == "nn":
                 return True
@@ -46,6 +66,15 @@ class ModelAnalyzer(ast.NodeVisitor):
 
     #
     def visit_ClassDef(self, node: ast.ClassDef) -> None:
+        """
+        Called when the ast visitor detects a definition of a class.
+
+        Args:
+            node (ast.ClassDef): The class node to visit
+
+        Raises:
+            NameError: In case there are two classes with the same name, we return an error
+        """
 
         # Check if the class inherits from torch.nn.Module
         if not self._is_torch_module_class(node):
@@ -55,7 +84,7 @@ class ModelAnalyzer(ast.NodeVisitor):
         block_name: str = node.name
         #
         if block_name in self.model_blocks:
-            raise UserWarning(f"ERROR : There are two classes with the same name !!!\nBad name : {block_name}\n")
+            raise NameError(f"ERROR : There are two classes with the same name !!!\nBad name : {block_name}\n")
 
         #
         self.model_blocks[block_name] = lc.ModelBlock(block_name=block_name)
@@ -71,40 +100,87 @@ class ModelAnalyzer(ast.NodeVisitor):
 
     #
     def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
-        #
+        """
+        Called when the ast visitor detects a definition of a function.
+
+        Args:
+            node (ast.FunctionDef): _description_
+        """
+
+        # Check if we are currently visiting a class or not
+        if not self.current_model_visit:
+            return  # Ignore all the standalone functions
+
+        # Check if we are visiting the Class Initialisation function, where there are the layers definition
         if node.name == "__init__":
             self._analyze_init_method(node)
-        #
+
+        # Check if we are visiting the Class Forward method, were there is the main control flow of the model block
         elif node.name == "forward":
             self._analyze_forward_method(node)
-        #
+
+        # Check if we are visiting another method of the class, we will store them temporaly, and clean thoses that aren't used in the model.
         else:
             self._analyse_other_method(node)
-        #
+
+        # To continue the AST visit
         self.generic_visit(node)
 
     #
     def _analyze_init_method(self, node: ast.FunctionDef) -> None:
+        """
+        _summary_
+
+        Args:
+            node (ast.FunctionDef): _description_
+        """
+
         #
         self.current_function_visit = "__init__"
         #
-        # TODO: Get the arguments of the class
+        # TODO: Get the argument of the current class definition
+        # TODO: to put here: self.model_blocks[block_name].block_parameters
+        pass
         #
         # TODO: Get the layers definitions of the class
         # TODO: Get the variables definitions of the class
-        # TODO: Analyze deeply the ModuleList and Sequential and create sub blocks for theses correctly
+        # TODO: Analyze deeply the ModuleList and Sequential and create sub blocks for theses correctly (their blocks names will be `BlockModuleList_{Parent_Block_Name}_{n°}` and `BlocSequential_{Parent_Block_Name}_{n°}`)
         pass
 
     #
     def _analyze_forward_method(self, node: ast.FunctionDef) -> None:
+        """
+        _summary_
+
+        Args:
+            node (ast.FunctionDef): _description_
+        """
+
         #
         self.current_function_visit = "forward"
         #
-        # TODO
+        # TODO: Get the arguments of the forward method
         pass
+
+        # TODO: Detect (create sub methods for each one):
+        # TODO:   - the variables initializations (lc.FlowControlVariableInit)
+        # TODO:   - the variables assignments (lc.FlowControlVariableAssignment)
+        # TODO:   - the basic arithmetics (ex: A = A + B), or decompose the chain of arithmetics in multiple basic arithmetic operations (ex: A = (A + B) - C -> TMP1 = A + B; A = TMP1 - C)
+        # TODO:   - the functions calls (lc.FlowControlFunctionCall or lc.FlowControlSubBlockFunctionCall)
+        # TODO:   - the layers calls (lc.FlowControlLayerPass)
+        # TODO:   - the returns (lc.FlowControlReturn)
+        # TODO:   - the conditions blocks -> Create sub-blocks for each condition branch, add a layer to the model block layers definitions of type lc.LayerCondition, and then add a flow instruction of type lc.FlowControlLayerPass
+        # TODO:   - the loops (lc.FlowControlForLoop or lc.FlowControlWhileLoop)
 
     #
     def _analyse_other_method(self, node: ast.FunctionDef) -> None:
+        """
+        _summary_
+
+        Args:
+            node (ast.FunctionDef): _description_
+        """
+
         #
         self.current_function_visit = node.name
         #
@@ -113,18 +189,77 @@ class ModelAnalyzer(ast.NodeVisitor):
 
     #
     def get_layer_type(self, func: ast.Expr) -> str:
+        """
+        _summary_
+
+        Args:
+            func (ast.Expr): _description_
+
+        Returns:
+            str: _description_
+        """
+
         if isinstance(func, ast.Attribute):
             return func.attr
         elif isinstance(func, ast.Name):
             return func.id
         return ""
 
+
+    # --------------------------------------------------------- #
+    # ----                 GENERIC VISITOR                 ---- #
+    # --------------------------------------------------------- #
+
     #
-    def generic_visit(self, node: ast.AST):
+    def generic_visit(self, node: ast.AST) -> None:
+        """
+        _summary_
+
+        Args:
+            node (ast.AST): _description_
+        """
+
         #
         # TODO: is there other things to check here ? (for instance function calls, or arithmetic operations / variable manipulation and other ?, maybe create other visit_... methods ?)
         #
         ast.NodeVisitor.generic_visit(self, node)
+
+
+    # --------------------------------------------------------- #
+    # ----            BASIC & EASY OPTIMISATIONS           ---- #
+    # --------------------------------------------------------- #
+
+    #
+    def basic_optimisations(self) -> None:
+        """
+        _summary_
+        """
+
+        #
+        # TODO: Search for basic optimisations like checking useless control flow instructions or optimisations of temporaries variables (if there is a way to reduce their numbers by checking the variables dependancy graph for instance)
+        # TODO: Clarify and explain what is done in this method
+        # TODO: Decompose the task in multiple sub methods for better code
+        #
+        pass
+
+
+    # --------------------------------------------------------- #
+    # ----                    CLEANING                     ---- #
+    # --------------------------------------------------------- #
+
+
+    #
+    def cleaning(self) -> None:
+        """
+        _summary_
+        """
+
+        #
+        # TODO: Cleaning unused functions in blocks (need a control flow analysis)
+        # TODO: Clarify and explain what is done in this method
+        # TODO: Decompose the task in multiple sub methods for better code
+        #
+        pass
 
 
 #
@@ -133,6 +268,10 @@ if __name__ == "__main__":
     #
     if len(sys.argv) != 2:
         raise UserWarning(f"Error: if you use this script directly, you should use it like that :\n  python {sys.argv[0]} path_to_model_script.py")
+
+    #
+    # TODO: add the support for the main model block argument that specifies which block is the main model block
+    pass
 
     #
     path_to_file: str = sys.argv[1]
