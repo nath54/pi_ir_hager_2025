edb26c9df9b25d733c314d9ca4b3e01d3358e037
Clement Desberg <clement@desberg.fr>
2025-03-12 13:00:49 +0100

    20250312 update_c_writer

--- c_writer.py
+++ c_writer.py
@@ -9,8 +9,20 @@ class CWriter:
         A class containing all methods to write the second intermediate langage basic operations in C++.
     """
     
-    def __init__(self, output_path : ):
-        self.output_file = open(output_path)
+    def __init__(self, output_path : str):
+        self.output_file = open(output_path,'w')
+    
+    def write(self, to_write : str):
+        """
+            Write to the output file.
+        Args:
+            to_write (str): the string to write to the output file.
+        """
+        self.output_file.write(to_write)
+    
+    
         
+    def __del__(self):
+        self.output_file.close()
 
 cwriter = CWriter(OUTPUT_PATH)
