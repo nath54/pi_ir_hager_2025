6ae0162a4ee26e8f9ecccac01c25f08f648e0d6f
nath54 <cerisara.nathan@gmail.com>
2025-04-09 15:22:20 +0200

    20250409 tests tensorflow-lite & executorch -> both didn't work

---
+++ Tests_Concurrents/test_torchscript/export_model.py
@@ -0,0 +1,66 @@
+#
+import numpy
+#
+import pickle
+import numpy as np
+import torch
+from torch import nn
+from torch import Tensor
+
+#
+from model_to_export import Model
+
+
+#
+def export_executorch_model(model: nn.Module, example_inputs: tuple) -> None:
+
+    # --- Standard ExecuTorch Export ---
+    try:
+
+        traced_script_module = torch.jit.trace(model, example_inputs)
+
+        # Save the TorchScript model
+        traced_script_module.save("traced_model.pt")
+
+    except Exception as e:
+        print(f"Export failed: {e}")
+        # Consider logging details or using specific exception handling
+
+
+#
+if __name__ == "__main__":
+
+    #
+    model: nn.Module = Model()
+
+    #
+    model.load_state_dict(torch.load("model_to_export_weights.pth", weights_only=True))
+
+    #
+    with open("model_to_export_example_data.pkl", "rb") as f:
+        example_data: list[np.ndarray] = pickle.load( f )
+
+    #
+    input_data1: Tensor = Tensor( example_data[0] ).unsqueeze(dim=0)
+    input_data2: Tensor = Tensor( example_data[1] ).unsqueeze(dim=0)
+
+    #
+    # print( f"\ninput_data : {input_data}\n" )
+    print( f"\ninput_data1 shape : {input_data1.shape}\n" )
+
+    #
+    print( f"\ninput_data2 : {input_data2}\n" )
+    print( f"\ninput_data2 shape : {input_data2.shape}\n" )
+
+    #
+    model.eval()
+
+    #
+    predictions: Tensor = model( input_data1 )
+
+    #
+    print( f"\npredictions : {predictions}\n" )
+    print( f"\npredictions shape : {predictions.shape}\n" )
+
+    #
+    export_executorch_model( model, (input_data1,) )
