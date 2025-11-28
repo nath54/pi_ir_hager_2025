cecabd573cb1e2b582b2cf069a365e9a7ffa114b
nath54 <cerisara.nathan@gmail.com>
2025-11-28 16:07:30 +0100

    Nathan: first push qui marche pas

--- export_model_on_stm32_with_gcc/main.c
+++ export_model_on_stm32_with_gcc/main.c
@@ -5,7 +5,8 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/cm3/systick.h>
 
-#include <network.h>
+#include "network.h"
+#include "network_data.h"
 
 // NOTE: Many Nucleo-144 boards map LD1/LD2/LD3 to PB0, PB7, PB14 respectively.
 // If your board uses a different mapping, update the port/pin defines below.
@@ -17,6 +18,12 @@
 #define LED3_PIN  GPIO14
 
 static volatile uint32_t system_millis = 0;
+
+// Neural network variables
+static ai_handle network = AI_HANDLE_NULL;
+static ai_u8 activations[AI_NETWORK_DATA_ACTIVATION_1_SIZE];
+static ai_i8 input_data[AI_NETWORK_IN_1_SIZE];   // 300 bytes: 30x10 matrix
+static ai_i8 output_data[AI_NETWORK_OUT_1_SIZE]; // 1 byte output
 
 void sys_tick_handler(void) {
 	system_millis++;
@@ -67,9 +74,39 @@ int main(void) {
 	gpio_setup();
 	systick_setup(0);
 
-    // Init model
-    // TODO: we need a ai_handle or similar thing that contains the model description.
-    // TODO: we need to create (if needed) & initialize the model (if needed).
+	// Initialize the neural network model
+	ai_network_params params = {
+		.params = AI_HANDLE_NULL,
+		.activations = AI_HANDLE_NULL
+	};
+
+	// Get the default params (includes weights)
+	if (!ai_network_data_params_get(&params)) {
+		// Error: blink red LED rapidly
+		for (;;) {
+			gpio_toggle(LED3_PORT, LED3_PIN);
+			delay_ms(100);
+		}
+	}
+
+	// Create the network
+	ai_error err = ai_network_create(&network, AI_NETWORK_DATA_CONFIG);
+	if (err.type != AI_ERROR_NONE) {
+		// Error: blink red LED
+		for (;;) {
+			gpio_toggle(LED3_PORT, LED3_PIN);
+			delay_ms(200);
+		}
+	}
+
+	// Initialize the network with activations buffer
+	if (!ai_network_init(network, &params)) {
+		// Error: blink red LED slowly
+		for (;;) {
+			gpio_toggle(LED3_PORT, LED3_PIN);
+			delay_ms(500);
+		}
+	}
 
 	uint8_t counter = 0;
 
@@ -84,12 +121,50 @@ int main(void) {
         // ON ALLUME LA LED ROUGE POUR DIRE QU'ON COMMENCE LE CALCUL
         gpio_set(LED3_PORT, LED3_PIN);
 
-        // ON VA METTRE LA FONCTION DE CALCUL, POUR  L'INSTANT ON FAIT UN PLACEHOLDER
-		delay_ms(1000);
+        // Generate a pseudo-random input vector (30x10 = 300 int8 values)
+        for (int i = 0; i < AI_NETWORK_IN_1_SIZE; i++) {
+            // Simple PRNG using system time and counter
+            input_data[i] = (ai_i8)((system_millis * 13 + counter * 7 + i * 3) % 256 - 128);
+        }
 
-        // TODO: generate a random vector of dimension (30, 10)
-        // TODO: run the model
-        // TODO: get the output. We will not use it for the moment, but later we will give it into a PIN of the STM32
+        // Prepare input buffer
+        ai_buffer ai_input[AI_NETWORK_IN_NUM] = {
+            AI_BUFFER_INIT(
+                AI_FLAG_NONE,
+                AI_BUFFER_FORMAT_S8,
+                AI_BUFFER_SHAPE_INIT(AI_SHAPE_BCWH, 4, 1, AI_NETWORK_IN_1_CHANNEL, 1, AI_NETWORK_IN_1_HEIGHT),
+                AI_NETWORK_IN_1_SIZE,
+                NULL,
+                input_data
+            )
+        };
+
+        // Prepare output buffer
+        ai_buffer ai_output[AI_NETWORK_OUT_NUM] = {
+            AI_BUFFER_INIT(
+                AI_FLAG_NONE,
+                AI_BUFFER_FORMAT_S8,
+                AI_BUFFER_SHAPE_INIT(AI_SHAPE_BCWH, 4, 1, AI_NETWORK_OUT_1_CHANNEL, 1, 1),
+                AI_NETWORK_OUT_1_SIZE,
+                NULL,
+                output_data
+            )
+        };
+
+        // Run the neural network inference
+        ai_i32 batch = ai_network_run(network, ai_input, ai_output);
+        
+        if (batch != 1) {
+            // Inference failed - blink red LED twice rapidly
+            for (int i = 0; i < 4; i++) {
+                gpio_toggle(LED3_PORT, LED3_PIN);
+                delay_ms(50);
+            }
+        }
+
+        // Get the output value (available in output_data[0])
+        // ai_i8 result = output_data[0];
+        // TODO: Use result for GPIO output or other purposes in the future
 
         // ON ALLUME LA LED VERTE POUR DIRE QU'ON A FINI LE CALCUL
         gpio_clear(LED3_PORT, LED3_PIN);
